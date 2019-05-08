24ff8b083bb38b2329b9ae05782b6f05cfa404b2
JakGradey <jacobgrady31@gmail.com>
2019-05-07 23:41:47 -0500

    updat

--- src/Score.java
+++ src/Score.java
@@ -5,22 +5,19 @@ import static java.nio.file.StandardOpenOption.*;
 public class Score {
     public static void main()
     {
-        int score2;
-        score2 =  Cell.score;
         Path file = Paths.get("C:\\Users\\jg318682\\Desktop\\MemeSweeper\\Score.txt");
         String s = "";
         byte[] data = s.getBytes();
         OutputStream output = null;
         try
         {
-            output = new
-                    BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            output = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
             output.write(data);
             output.flush();
             output.close();
         }
         catch(Exception e){
-            System.out.println(score2);
+            System.out.println(Cell.score);
         }
     }
 }
